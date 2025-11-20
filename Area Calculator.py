4c3529d6dccaa4a98894a43f439a3eeff4b2598d
Draymond3535 <rayann.kanga@enkoschools.com>
2025-11-20 15:04:58 +0000

    Update Area Calculator.py

--- Area Calculator.py
+++ Area Calculator.py
@@ -1,35 +1,44 @@
-import math 
-print("Area Calculator 📐")
+import math
+
+print("Calculateur de surface 📐")
 print("1) Triangle")
-print("2) Rectangle") 
-print("3) Square")
-print("4) Circle")
-print("5)Quit")
-fig= int(input("Which Shape:"))
-a=1
-b=2
-c=3
-d=4
-e=5
-if fig==a :
-  ba= float(input("Base:"))
-  he= float(input("Height:"))
-  area= (ba*he)/2
-  print("The area of the triangle is", area)
-elif fig ==b :
-  le= float(input("Length:"))
-  wi= float(input("Width:"))
-  area_2= le*wi
-  print("The area of the rectangle is", area_2)
-elif  fig == c :
-  co= float(input("Side :"))
-  area_3= co**2
-  print("The area of the square is ", area_3)
+print("2) Rectangle")
+print("3) Carré")
+print("4) Cercle")
+print("5) Quitter")
+
+fig = int(input("Quelle forme choisissez-vous : "))
+
+a = 1
+b = 2
+c = 3
+d = 4
+e = 5
+
+if fig == a:
+    ba = float(input("Base : "))
+    he = float(input("Hauteur : "))
+    area = (ba * he) / 2
+    print("La surface du triangle est :", area)
+
+elif fig == b:
+    le = float(input("Longueur : "))
+    wi = float(input("Largeur : "))
+    area_2 = le * wi
+    print("La surface du rectangle est :", area_2)
+
+elif fig == c:
+    co = float(input("Côté : "))
+    area_3 = co ** 2
+    print("La surface du carré est :", area_3)
+
 elif fig == d:
-  ra= float(input("Radius:"))
-  area_4= math.pi * (ra**2)
-  print("The area of the circle is ",area_4,)
+    ra = float(input("Rayon : "))
+    area_4 = math.pi * (ra ** 2)
+    print("La surface du cercle est :", area_4)
+
 elif fig == e:
-  print("Good bye !")
-else :
-  print("Error . Please try again.")
+    print("Au revoir ! 👋")
+
+else:
+    print("Erreur. Veuillez réessayer.")
